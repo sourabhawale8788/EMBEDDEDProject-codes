0436411f4851a5c548742e7e871c833d2bdb2a07
sourabhawale8788 <sourabhawale8788@gmail.com>
2026-05-15 23:23:36 +0530

    ADC-Based LED Control using LPC2148 code in c as well as keil project file
    both file is same

---
+++ ADCPROGRAM.c
@@ -0,0 +1,34 @@
+// NAME : SOURABH SUNIL AWALE
+// PRN  : 22UET005
+
+#include <LPC214X.H>
+int adc_get_val(void);
+int main ()
+{
+	int a;
+	PINSEL0=0x30000;
+	IODIR0=IODIR0 | 0x10;
+	AD1CR=0x200104;
+	while(1)
+	{
+		a = adc_get_val();
+		if(a>512)
+		{
+			IOSET0=0x10;
+		}
+		else
+		{
+			IOCLR0=0x10;
+		}
+	}
+}
+
+int adc_get_val()
+{
+	int val ;
+	AD1GDR = AD1GDR & 0X7EfEfff;
+	AD1CR = AD1GDR | (1<<24);
+	while (! (AD1GDR & (1<<31)));
+	val =(AD1GDR & 0xffc0 )>>6;
+	return val ;
+}
